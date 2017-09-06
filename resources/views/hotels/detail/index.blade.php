71be45fb182be2e9d16daddef7558581fc381318
fateen980 <fateen_khaled@hotmail.com>
2017-09-07 00:22:30 +0300

    Lobby detail Page

--- resources/views/hotels/detail/index.blade.php
+++ resources/views/hotels/detail/index.blade.php
@@ -1,13 +1,12 @@
 @extends('layout.default')
 
 @section('content')
-
     @include('layout.nav.nav')
     @include('layout.nav.breadcrumb',[
         'routes'  =>
                 [
                     0 => ['url'   =>  'categoryController@index','title' => 'Home' ],
-                    1 => ['url'   =>  'hotelController@index','title' => 'Hotels' ]
+                    1 => ['url'   =>  'hotelController@index','title'    => 'Hotels' ]
                 ] ,
                           'name'  => @isset($detail) ? $detail->nameEng : ''
 ])
