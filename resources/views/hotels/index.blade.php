0e982ece430577bb731d8c008b3e5d86b85bf92c
fateen980 <fateen_khaled@hotmail.com>
2017-09-07 00:25:40 +0300

    Lobby detail Page

--- resources/views/hotels/index.blade.php
+++ resources/views/hotels/index.blade.php
@@ -5,6 +5,13 @@
 @section('content')
 
     @include('layout.nav.nav')
+    @include('layout.nav.breadcrumb',[
+    'routes'  =>
+            [
+                0 => ['url'   =>  'categoryController@index','title' => 'Home' ]
+            ] ,
+            'name'  =>  'Hotels'
+])
     @include('hotels.sideSearch')
     @include('hotels.bodySearch',['loadJs' => 1])
 
